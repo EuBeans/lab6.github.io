daeb2f3caa8df627ef58dc838a29ac10eebd8840
Atrex11 <jsfei069@uottawa.ca>
2020-06-30 17:00:49 -0400

    initial

---
+++ public/assets/niceSurvey.js
@@ -0,0 +1,10 @@
+var slider = document.getElementById("sliderLocorice");
+var output = document.getElementById("sliderOutput");
+output.innerHTML = slider.value; // Display the default slider value
+
+
+// Update the current slider value (each time you drag the slider handle)
+slider.oninput = function() {
+  output.innerHTML = this.value;
+  console.log(this.value);
+}
